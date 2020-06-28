eb42d1b7eb20d675dbe47e0891b454a2241f874c
Cameron Brien <cameron@brien.ca>
2020-06-27 22:05:52 -0400

    #1 Boilerplate to kick off the lox executable

--- src/main/com/lox/Lox.java
+++ src/main/com/lox/Lox.java
@@ -1,33 +1,50 @@
 package com.lox;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class Lox
-{
-    public static void main(String[] args) throws IOException
-    {
-        if (args.length > 1)
-        {
+public class Lox {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1){
             System.out.println("Usage: jlox [script_to_execute]");
             System.exit(64);
         }
-        else if (args.length == 1) // run a script
-        {
+        else if (args.length == 1) { // run a script
             runFile(args[0]);
         }
-        else // run in interactive mode
-        {
+        else { // run in interactive mode
+
             runPrompt();
         }
     }
 
-    public static void runFile(String s)
-    {
-
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
     }
 
-    public static void runPrompt()
-    {
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        boolean done = false;
+        while (!done) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line != null) {
+                run(line);
+            }
+            else {
+                done = true;
+            }
+        }
+    }
+
+    private static void run(String toExec) {
 
     }
 
