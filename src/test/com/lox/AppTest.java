3588c191d595075b6a4654c6d76e990011ff0616
Cameron Brien <cameron@brien.ca>
2020-06-27 21:53:46 -0400

    #3 Stub test to ensure that gradle build is working

---
+++ src/test/com/lox/AppTest.java
@@ -0,0 +1,11 @@
+package com.lox;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void testAppHasAGreeting()
+    {
+        assertTrue("app should have a greeting", true);
+    }
+}
