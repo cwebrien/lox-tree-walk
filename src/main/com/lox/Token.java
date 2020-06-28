cc229ff56736333826246c303f554ecdb10f0e42
Cameron Brien <cameron@brien.ca>
2020-06-28 16:23:30 -0400

    #7 Stub for tokenization

---
+++ src/main/com/lox/Token.java
@@ -0,0 +1,31 @@
+package com.lox;
+
+
+public class Token {
+    private final TokenType type;
+    private final String lexeme;
+    private final Object literal;
+    private int line;
+
+    /**
+     * A token is defined by its type, underlying data and its location in the execution script or interactive session.
+     * @param type Token type
+     * @param lexeme The actual string value of the lexeme
+     * @param literal If it's a literal, we might as well inflate it now
+     * @param line Line location of the lexeme
+     */
+    public Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    /**
+     * Stringification of the token.
+     * @return A string representation.
+     */
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
