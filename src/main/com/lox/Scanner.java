7249c174138ac3b8d06525d50f681027c1d9c678
Cameron Brien <cameron@brien.ca>
2020-07-02 21:22:39 -0400

    #7 First cut of tokenization for double-char tokens

--- src/main/com/lox/Scanner.java
+++ src/main/com/lox/Scanner.java
@@ -30,10 +30,15 @@ public class Scanner {
         return tokens;
     }
 
+    /**
+     * Pulls one or more characters to lex the next token.
+     * @return
+     */
     private Token getNextToken() {
         char c = getNextChar();
         TokenType tokenType = null;
         switch (c) {
+            // Single-character tokens
             case '(': tokenType = TokenType.LEFT_PAREN; break;
             case ')': tokenType = TokenType.RIGHT_PAREN; break;
             case '{': tokenType = TokenType.LEFT_BRACE; break;
@@ -44,12 +49,25 @@ public class Scanner {
             case '+': tokenType = TokenType.PLUS; break;
             case ';': tokenType = TokenType.SEMICOLON; break;
             case '*': tokenType = TokenType.STAR; break;
+
+            // Double-character tokens (potentially)
             case '!': tokenType = (match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
             case '=': tokenType = (match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
             case '<': tokenType = (match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
             case '>': tokenType = (match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+
+            // Possibly a comment
+            case '/':
+                if (match('/')) {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) getNextChar();
+                } else {
+                    tokenType = TokenType.SLASH;
+                }
+                break;
         }
 
+        // Handle null tokens, i.e. errors
         if(tokenType == null) {
             Lox.error(line, "Unexpected character.");
             return null;
@@ -73,10 +91,27 @@ public class Scanner {
         return true;
     }
 
+    /**
+     * Looks forward one character.
+     * @return The next character or the null-terminating character if we're done reading.
+     */
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    /**
+     * Pulls the next character and moves us forward in the buffer.
+     * @return The next character (with side effects)
+     */
     private char getNextChar() {
         return source.charAt(current++);
     }
 
+    /**
+     * We've completed reading the source code buffer.
+     * @return
+     */
     private boolean isAtEnd() {
         return current >= source.length();
     }
