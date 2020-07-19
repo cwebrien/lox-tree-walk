aeed773e3193c4ab54f83e82a9b32639d7f4d5ec
Cameron Brien <cameron@brien.ca>
2020-07-18 20:38:42 -0400

    #22 Added test to improve coverage of Token

--- src/test/com/lox/TestToken.java
+++ src/test/com/lox/TestToken.java
@@ -4,6 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestToken {
+    /**
+     * Tests string conversion of Token class.
+     */
     @Test
     public void testToString() {
         Token t = new Token(TokenType.IDENTIFIER, "some_variable_name", null, 5);
