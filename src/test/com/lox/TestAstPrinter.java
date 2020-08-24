befdeb63ceca7bed16b86c270469834b49f0165c
Cameron Brien <cameron@brien.ca>
2020-08-24 12:45:19 -0400

    Remove test that isn't relevant here.

--- src/test/com/lox/TestAstPrinter.java
+++ src/test/com/lox/TestAstPrinter.java
@@ -23,24 +23,4 @@ public class TestAstPrinter {
         String result = printer.expressionAsString(TEST_EXPRESSION);
         assertEquals("(* (- 3.14) (grouping 2.718))", result);
     }
-
-    /**
-     * Test isUseful().
-     */
-    @Test
-    public void testIsUseful() {
-        // Comments are not useful; everything else is
-        TokenType[] allTypes = TokenType.class.getEnumConstants();
-        for(TokenType t : allTypes) {
-            if(t == TokenType.COMMENT) {
-                assertFalse(TokenType.isUseful(new Token(t, "", null, -1)));
-            }
-            else {
-                assertTrue(TokenType.isUseful(new Token(t, "", null, -1)));
-            }
-        }
-
-        // Special case
-        assertFalse(TokenType.isUseful(null));
-    }
 }
