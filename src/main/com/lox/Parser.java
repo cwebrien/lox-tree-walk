7db683267fd71b0fffcd9fda2e9c2786cf956cc7
Cameron Brien <cameron@brien.ca>
2020-08-25 20:56:10 -0400

    #34 Add some more helper methods

--- src/main/com/lox/Parser.java
+++ src/main/com/lox/Parser.java
@@ -2,9 +2,9 @@ package com.lox;
 
 import java.util.List;
 
-class Parser {
+public class Parser {
     private final List<Token> tokens;
-    private int currentToken = 0;
+    private int currentIndex = 0;
 
     /**
      * Constructs a parser from an ordered list of tokens.
@@ -68,5 +68,63 @@ class Parser {
     private Expression primary() {
         Expression expr = null;
         return expr;
+    }
+
+    private Token advance() {
+        Token token = tokens.get(currentIndex);
+        currentIndex++;
+
+        return token;
+    }
+
+    /**
+     * Return the current token without advancing the token index.
+     * @return The current token
+     */
+    private Token peek() {
+        return tokens.get(currentIndex);
+    }
+
+    /**
+     * Return the previously consumed token.
+     * @return The previously consumed token
+     */
+    private Token previous() {
+        return tokens.get(currentIndex - 1 );
+    }
+
+    /**
+     * We're at the end if we explicitly see an EOF token, or if we have consumed all of the tokens in the list.
+     * @return True if we've consumed all of the tokens up to the EOD; otherwise false
+     */
+    private boolean isAtEnd() {
+        return currentIndex >= tokens.size() || peek().getType() == TokenType.EOF;
+    }
+
+    /**
+     * Returns true and moves forward if the current token matches any of the types specified.
+     * @param types A set of types for matching
+     * @return True if and only if we find a match
+     */
+    private boolean matchAny(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks for a match of the current token to the type specified.
+     * @param type To check against
+     * @return True if and only if the current token being processed is of the specified type
+     */
+    private boolean check(TokenType type) {
+        if (isAtEnd()) {
+            return false;
+        }
+        return peek().getType() == type;
     }
 }
